bd436dee63479f00b8af2fa788495474963b510b
Smruti <lenkasmruti123@gmail.com>
2021-09-25 17:08:57 +0530

    Added: EmpWageuc2

---
+++ EmpWageuc2.java
@@ -0,0 +1,21 @@
+package Employee;
+
+public class EmpWageuc2 {
+	public static void main(String[] args) {
+		int IS_PRESENT = 1;
+		 int FULL_TIME = 8;
+		    int WAGE_PER_HR = 20;
+		    int dailyWage = 0;
+		    //calculation
+		    int empCheck = (int) Math.floor(Math.random() * 10) % 2;		     
+		    if (empCheck == IS_PRESENT) {
+		        System.out.println(" Employee is Present");
+		        dailyWage = FULL_TIME * WAGE_PER_HR;
+		        System.out.println("the Salary is " + dailyWage);
+		    } else {
+		        System.out.println(" Employee is Absent");
+		 
+	}
+
+	}
+}
