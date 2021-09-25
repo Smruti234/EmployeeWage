7e8a77523d21012ca3241cb66c222652556a6206
Smruti234 <89955654+Smruti234@users.noreply.github.com>
2021-09-25 17:35:04 +0530

    Update EmpWageuc1.java

--- EmpWageuc1.java
+++ EmpWageuc1.java
@@ -1,17 +1,24 @@
 package Employee;
 
-public class EmpWageuc1 {
-	
-		public static void main(String[] args) {
-			int IS_FULL_TIME = 1;
-			double empCheck = Math.floor(Math.random() * 10) % 2;
-			if (empCheck == IS_FULL_TIME)
-				System.out.println("Employee is Present");
-			else
-				System.out.println("Employee is Absent");
-		}
-		
+
+public class EmpWageuc2 {
+	public static void main(String[] args) {
+		int IS_PRESENT = 1;
+		 int FULL_TIME = 8;
+		    int WAGE_PER_HR = 20;
+		    int dailyWage = 0;
+		    //calculation
+		    int empCheck = (int) Math.floor(Math.random() * 10) % 2;		     
+		    if (empCheck == IS_PRESENT) {
+		        System.out.println(" Employee is Present");
+		        dailyWage = FULL_TIME * WAGE_PER_HR;
+		        System.out.println("the Salary is " + dailyWage);
+		    } else {
+		        System.out.println(" Employee is Absent");
+		 
 	}
-	
+
+	}
+}
 
 
